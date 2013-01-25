5fe46697036232c59ebaafebe352ca8eb70f5179
Aneesh Dogra <lionaneesh@gmail.com>
2013-01-25 08:19:52 +0530

    Improve the comments form.

--- comments.php
+++ comments.php
@@ -72,6 +72,19 @@
 		<p class="nocomments"><?php _e( 'Comments are closed.', 'portraiture' ); ?></p>
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+
+	<?php
+	$commenter = wp_get_current_commenter();
+	$req = get_option( 'require_name_email' );
+	$aria_req = ( $req ? " aria-required='true'" : '' );
+
+	$comments_args = array(
+		'fields' => apply_filters( 'comment_form_default_fields', array(
+		'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name', 'domainreference' ) . ( $req ? ' (*) ' : '' ) . '</label> ' . '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
+		'email' => '<p class="comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . ( $req ? ' (*) ' : '' ) . '</label> ' . '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>',
+		'url' => '<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' . '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>' ) ) );
+
+	comment_form($comments_args);
+?>
 
 </div><!-- #comments .comments-area -->
